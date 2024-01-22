6b1ee3f84d4d35400f8c468a62f02f0c9a2488e7
samoanspam <thalpatane@byui.edu>
2024-01-22 14:08:03 -0700

    ddd

--- testing/test.py
+++ testing/test.py
@@ -1,28 +1,5 @@
-# What are passed as value?: string, integer, float, bool
-# What are passed as references?: list, dict, set, objects.
+import multiprocessing as mp
 
-import threading
-import time
+test = mp.cpu_count()
 
-class Display_Hello(threading.Thread):
-    def __init__(self, number):
-        # this is the consturctor
-        threading.Thread.__init__(self)
-
-        # this is where you creawte variables
-        self.number = number
-
-    def run(self):
-        time.sleep(self.number)
-        print(f'Hello World: {self.number}')
-
-
-if __name__ == '__main__':
-    hello1 = Display_Hello(2)
-    hello2 = Display_Hello(1)
-
-    hello1.start()
-    hello2.start()
-
-    hello1.join()
-    hello2.join()
+print(test)
